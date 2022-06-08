6d850bbd7a5febd930711c0c528db4d15369e49f
Emma Wenham <ewenham3@gmail.com>
2022-06-08 20:36:12 +0100

    changed db query

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,13 +1,14 @@
-const getDb = require('../src/services/db');
+const getDb = require('../services/db');
 
 exports.create = async (req, res) => {
   const db = await getDb();
   const { name, genre } = req.body;
 
   try {
-    await db.query(`
-    INSERT INTO Artist (name, genre)
-    VALUES ('${name}, ${genre}')`);
+    await db.query('INSERT INTO Artist (name, genre) VALUES (?, ?)', [
+      name,
+      genre, 
+    ]);
 
     res.sendStatus(201);
   } catch (err) {
